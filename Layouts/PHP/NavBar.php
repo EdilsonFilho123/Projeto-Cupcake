2b2542ea1ebb52b2cbc7781a4b22951be76e120c
edilson <edilsonlinefilho@gmail.com>
2022-05-03 11:47:20 -0300

    teste inclusão

---
+++ Layouts/PHP/NavBar.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="../../View/Home/">Home</a></li>
+        <li><a href="../../View/Cardapio/">Cardápio</a></li>
+        <li><a href="../../View/Customizar/">Customizar</a></li>
+        <li><a href="../../View/Sobre/">Sobre</a></li>
+    </ul>
+</nav>
---
+++ View/Sobre/Index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sobre</title>
+        <link rel="stylesheet" href="../../Layouts/CSS/StyleBase.css">
+        <link rel="stylesheet" href="../../Layouts/CSS/StyleNavBar.css">
+    </head>
+
+    <body>
+        <header>
+            <?php include("../../Layouts/PHP/NavBar.php"); ?>
+        </header>
+
+        <aside class="aside-left">
+            
+        </aside>
+
+        <main>
+            
+        </main>
+
+        <aside class="aside-right">
+            
+        </aside>
+
+        <footer>
+            
+        </footer>
+    </body>
+</html>
