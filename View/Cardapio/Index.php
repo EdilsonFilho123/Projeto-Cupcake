39b77b8af5036377676b8a40a2c58d3f9bdc2f7c
EdilsonFilho123 <edilsonlinefilho@gmail.com>
2022-05-30 19:41:30 -0300

    Inclução de Midias e de conteudo das paginas Sobre e Cardapio

--- View/Cardapio/Index.php
+++ View/Cardapio/Index.php
@@ -21,55 +21,60 @@
         </aside>
 
         <main>
+            <br><br>
+            <h1>CUPCAKES</h1>
+            <br>
+
             <div class="divisaoCardapio">
+
                 <div class="cardapioLeft">
 
                     <div class = "divcardesq">
                         <h4>CENOURA</h4>
                         <p>Cenoura com gotas de chocolate, recheio de nutella e cobertura de brigadeiro.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>LIMÃO</h4>
                         <p>Massa de limão, recheio de Lemon Curd (creme azedinho de limão) e cobertura de glacê ou brigadeiro ou merengue.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>RED VELVET</h4>
                         <p>Cupcake clássico com massa de baunilha vermelha e cobertura de glacê de cream cheese. *Não tem recheio!</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>MARACUJÁ</h4>
                         <p>Massa de maracujá crocante, recheio de brigadeiro de maracujá e cobertura de brigadeiro branco.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>BANANA CRUMBLE</h4>
                         <p>Massa de banana crumble com recheio de doce de leite e cobertura de nutella.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>MAÇÃ CRUMBLE</h4>
                         <p>Massa de maçã crumble com recheio de doce de leite e cobertura de nutella.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>MORANGO COM 2 BRIGADEIROS</h4>
                         <p>Massa de morango, recheio de brigadeiro branco com geléia de morango e cobertura de brigadeiro tradicional e brigadeiro de morango.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>LARANJA</h4>
                         <p>Massa de laranja com recheio de creme de laranja e cobertura de brigadeiro de chocolate.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>NOZES COM BABA DE MOÇA</h4>
                         <p>Massa de nozes, recheio de baba de moça (ou doce de leite) e cobertura de brigadeiro branco (ou merengue).</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>CHOCOLATE AO CUBO</h4>
                         <p>Massa de chocolate com recheio de chocolate fudge (calda meio amarga deliciosa!) e cobertura de brigadeiro com chocolate em cubinhos.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>CHOCOCUP</h4>
                         <p>Massa de chocolate, recheio de brigadeiro e cobertura de merengue.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>FRUTAS VERMELHAS</h4>
                         <p>Massa de baunilha com recheio de creme de frutas vermelhas e cobertura de brigadeiro preto com raspas de chocolate.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>BLUEBERRY</h4>
                         <p>Massa de chocolate ou baunilha, recheio de creme de blueberry e cobertura de brigadeiro branco ou preto.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>BAUNILHA</h4>
                         <p>Massa de baunilha, recheio de brigadeiro branco e cobertura de glacê de baunilha.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>PISTACHE</h4>
                         <p>Massa de baunilha ou chocolate com recheio de creme de pistache e cobertura de brigadeiro branco ou preto.</p>
-                        <br>
+                        <p>.....................................................................................................................................</p>
                         <h4>ROMEU & JULIETA</h4>
                         <p>Massa baunilha com toque de canela, recheio de goiabada mole e cobertura de glacê de cream cheese.</p>
                     </div>
@@ -78,9 +83,50 @@
                 <div class="cardapioRight">
 
                     <div class = "divcarddir">
-                        <p>uvbheiurbviuerwoberfngsrtwrnmkbnielrij</p>
-                    </div>
-
+                        <h4>OVOMALTINE</h4>
+                        <p>Massa de ovomaltine, recheio de chocolate fudge e cobertura de brigadeiro branco ou preto com ovomaltine polvilhado.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>PAÇOCA</h4>
+                        <p>Massa de paçoca com recheio de brigadeiro branco e cobertura de brigadeiro preto com paçoca salpicada.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>CAFÉ COM DOCE DE LEITE</h4>
+                        <p>Massa de café com recheio de doce de leite e cobertura de glacê de cream cheese com toque de cacau.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>CEREJA COM BRIGADEIRO</h4>
+                        <p>Massa de baunilha vermelha com recheio de brigadeiro de cereja e cobertura de brigadeiro tradicional.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>PRESTÍGIO</h4>
+                        <p>Massa de chocolate com recheio de beijinho de coco e cobertura de chocolate com coco ralado.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>BRIGADEIRO COM MORANGO</h4>
+                        <p>Massa de chocolate ou baunilha com recheio e cobertura de brigadeiro de morango.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>BEIJINHO</h4>
+                        <p>Massa de baunilha com recheio de beijinho de coco e cobertura branca ou preta.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>BRIGADEIRO</h4>
+                        <p>Massa de chocolate com recheio e cobertura de brigadeiro.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>MENTA COM CHOCOLATE</h4>
+                        <p>Massa de chocolate, recheio de brigadeiro de menta e cobertura de glacê de menta.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>DEVIL'S FOOD</h4>
+                        <p>Massa amarga de chocolate com recheio e cobertura de chocolate glacê. Divinamente um pecado!!!</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>CUPCHURROS</h4>
+                        <p>Massa de baunilha dourada com recheio e cobertura de doce de leite, açúcar e canela.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>MILHO COM GLACÊ DE GOIABADA</h4>
+                        <p>Massa de milho, recheio de brigadeiro suave de goiabada e cobertura de glacê com pipoca caramelada no topo.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>ABACAXI COM COCO</h4>
+                        <p>Massa de baunilha, recheio de creme de abacaxi e cobertura de beijinho de coco com abacaxi em calda.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>CAIPIRINHA</h4>
+                        <p>Massa de limão, recheio de brigadeiro branco com cachaça e glacê de baunilha e limão.</p>
+                        <p>.....................................................................................................................................</p>
+                        <h4>PISTACHE</h4>
+                        <p>Massa de baunilha ou chocolate com recheio de creme de pistache e cobertura de brigadeiro branco ou preto.</p>
                 </div>
             </div>
         </main>
--- View/Sobre/Index.php
+++ View/Sobre/Index.php
@@ -8,6 +8,7 @@
         <link rel="shortcut icon" href="../../Midia/Imgs/cupcake1.png" type="image/x-icon">
         <link rel="stylesheet" href="../../Layouts/CSS/StyleBase.css">
         <link rel="stylesheet" href="../../Layouts/CSS/StyleNavBar.css">
+        <link rel="stylesheet" href="StyleSobre.css">
     </head>
 
     <body>
@@ -20,7 +21,44 @@
         </aside>
 
         <main>
+            <br><br>
+            <h1>SOBRE NÓS</h1>
+            <br>
+
+            <div class="divisaoSobre">
+                
+                <div class="SobreLeft">
+                    <p>Somos uma empresa de cupcakes nascida do sonho de 6 jovens. Temos nossa matriz em Limeira - SP, mas já possuímos 3 filiais pelo estado, uma em Piracicaba, uma em Cordeirópolis e uma em Rio Claro.</p>
+                </div>
+
+                <div class="SobreRight">
+                    <p>Nossos cupcakes são feitos com muito amor, mas também com muito cuidado e técnica. Venha visitar nossas lojas ou peça essas delicinhas na sua casa!!</p>
+                </div> 
+
+            </div> 
+            <p>....................................................................................................................................................................................................................................</p>
+            <br>
+            <h1>CONTATOS</H1>
+            <br><br>
+
+            <div class="divisaoSobre2">
+                    <a href="https://www.facebook.com/einstein.limeira">
+						<img src="../../Midia/Imgs/facebook.png" alt="Logo do Facebook">
+					</a>
+
+					<a href="https://www.instagram.com/einsteinlimeira/">
+						<img src="../../Midia/Imgs/instagram.png" alt="Logo do Instagram">
+					</a>
+
+                    <a href="https://mobile.twitter.com/EinsteinLimeira?p=s">
+						<img src="../../Midia/Imgs/twitter.png" alt="Logo do Twitter">
+					</a>
+            </div> 
             
+            <br><br>
+                <div class="mapa">
+					<p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.6746879685693!2d-47.39956958449428!3d-22.553855831043762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8809056a21881%3A0x1c82f0fb0c889ee8!2sFaculdades%20Integradas%20Einstein%20de%20Limeira!5e0!3m2!1spt-BR!2sbr!4v1637893147677!5m2!1spt-BR!2sbr" width="600" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p>
+				</div>
         </main>
 
         <aside class="aside-right">
